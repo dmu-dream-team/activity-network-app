45994a3294f88599f8226d4ba291e51de01fa342
Hendrik Peter van der Meulen <peter@picoreclame.nl>
2019-02-22 21:19:51 +0100

    Interest seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,18 @@ locations = YAML.safe_load(locations_file)['locations']
 locations.each do |location|
   next if Location.find_by(nyko_id: location['nyko_id'])
 
-  puts "inserting #{location['friendly_name']}"
-  puts "inserting failed for #{location['nyko_id']}" unless Location.create(location)
+  puts "Inserting #{location['friendly_name']}"
+  puts "Inserting failed for #{location['nyko_id']}" unless Location.create(location)
+end
+
+## Interests
+
+interests_file = File.read("#{Rails.root}/db/interests_data_seed.yml")
+interests = YAML.safe_load(interests_file)['interests']
+
+interests.each do |interest|
+  next if Interest.find_by(name: interest['name'])
+
+  puts "Inserting #{interest['name']}"
+  puts "Inserting failed for #{interest['name']}" unless Interest.create(interest)
 end

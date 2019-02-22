76c7ffd2149275699a353d86aac11752c5e8333e
Hendrik Peter van der Meulen <peter@picoreclame.nl>
2019-02-22 21:08:28 +0100

    Making things optional

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@
 
 class User < ApplicationRecord
   has_and_belongs_to_many :interests
-  belongs_to :event
-  belongs_to :cluster
-  belongs_to :location
+  belongs_to :event, optional: true
+  belongs_to :cluster, optional: true
+  belongs_to :location, optional: true
   validates :phone_number, presence: true
 end

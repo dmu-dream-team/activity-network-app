5f2598f321a95a0b9bf86b6090d6a6fbd1785b5d
Stefan Wallin <stefan.w@festiz.com>
2019-02-23 03:57:37 +0100

    Fake message sender

---
+++ app/models/fake_messages.rb
@@ -0,0 +1,26 @@
+class FakeMessages
+  def self.all
+    [
+      "Hej Agda! Vad kul att du känner dig redo att lära känna nya vänner. För att ditt möte ska bli så bra som möjligt så behöver du först svara på några frågor så vi kan matcha dig med rätt personer.\nFörst måste vi vet information om detta telefonnummer stämmer.\n\nHeter du Agda? (Svara Ja eller nej i textfältet nedanför)",
+      # JA
+      "Träffar du helst en person eller en grupp av människor?\nA) En person\nB) Flera personer samtidigt?",
+      # B
+      "Vad vill du träffa för andra människor?\nA) Spelar ingen roll\nB) Personer med liknade intressen",
+      # B
+      "Vad vill du göra?\nA) Träffas och äta något gott\nB) Prova en ny aktivitet som jag kanske inte provat tidigare\nC) Träffas och utföra en specifik aktivitet som jag tycker om",
+      # A
+      "När vill du träffa andra människor?\nA) Direkt\nB) Kvällar och helger\nC) När som helst",
+      # B
+      "Är du okej med att träffa personer som inte pratar så bra svenska?\nA) Ja\nB) Nej",
+      # A
+      "Vi matchar dig nu med andra personer\n\nDetta kan ta upp till en minut",
+      #
+      "Vi har matchat dig 3 andra personer för en lunch på Espresso house. Ni träffas utanför dörren klockan 12 på tordag. Passar denna tid för dig?\nSvara JA/NEJ",
+      # JA
+      "Du har accepterat inbjudan. Vi återkommer när de andra har svarat.",
+      "Oskar har accepterat inbjudan. Ni är nu 2 stycken som har accepterat inbjudan.",
+      "Dagny har nu accepterat inbjudan. Ni är nu 3 stycken som har accepterat inbjudan.",
+      "Erik har nu accepterat inbjudan. \nAlla som är inbjudna har accepterat.\n\nEfter träffen kommer ni får möjlighet att dela kontaktuppgifter med varandra."
+    ]
+  end
+end
